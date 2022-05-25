1563c446af25ef93dc21e80972e7bbf9a7d97595
Mathis Engelbart <mathis.engelbart@gmail.com>
2022-05-25 23:23:19 +0200

    Remove unused code

--- RtpQueue.h
+++ RtpQueue.h
@@ -42,43 +42,4 @@ public:
     virtual int getSizeOfLastFrame() = 0;
 };
 
-class RtpQueueItem {
-public:
-    RtpQueueItem();
-    char packet[2000];
-    int size;
-    unsigned short seqNr;
-    float ts;
-    bool used;
-};
-
-const int kRtpQueueSize = 1024;
-class RtpQueue : public RtpQueueIface {
-public:
-    RtpQueue();
-
-    void push(void *rtpPacket, int size, unsigned short seqNr, float ts);
-    bool pop(void *rtpPacket, int &size, unsigned short &seqNr);
-    int sizeOfNextRtp();
-    int seqNrOfNextRtp();
-    int bytesInQueue(); // Number of bytes in queue
-    int sizeOfQueue();  // Number of items in queue
-    float getDelay(float currTs);
-    bool sendPacket(void *rtpPacket, int &size, unsigned short &seqNr);
-    void clear();
-    int getSizeOfLastFrame() {return sizeOfLastFrame;};
-    void setSizeOfLastFrame(int sz) {sizeOfLastFrame=sz;};
-    void computeSizeOfNextRtp();
-
-    RtpQueueItem *items[kRtpQueueSize];
-    int head; // Pointer to last inserted item
-    int tail; // Pointer to the oldest item
-    int nItems;
-    int sizeOfLastFrame;
-
-    int bytesInQueue_;
-    int sizeOfQueue_;
-    int sizeOfNextRtp_;
-};
-
 #endif
--- RtpQueueCGO.h
+++ RtpQueueCGO.h
@@ -7,9 +7,12 @@
 #ifndef RTP_QUEUE_CGO
 #define RTP_QUEUE_CGO
 
+#include "RtpQueue.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
+
 
     void goClear(int);
     int goSizeOfNextRtp(int);
@@ -22,17 +25,6 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-
-class RtpQueueIface {
-public:
-    virtual void clear() = 0;
-    virtual int sizeOfNextRtp() = 0;
-    virtual int seqNrOfNextRtp() = 0;
-    virtual int bytesInQueue() = 0; // Number of bytes in queue
-    virtual int sizeOfQueue() = 0;  // Number of items in queue
-    virtual float getDelay(float currTs) = 0;
-    virtual int getSizeOfLastFrame() = 0;
-};
 
 class RtpQueueCGO : public RtpQueueIface {
 public:
